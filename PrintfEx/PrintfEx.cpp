bc4ac649a39451f583213a97749f9056af438d59
yuexiang <yuexiang@cherilead.com>
2015-08-18 10:24:45 +0800

    丰富printf信息

--- PrintfEx/PrintfEx.cpp
+++ PrintfEx/PrintfEx.cpp
@@ -2,23 +2,48 @@
 
 VOID
 	CPrintfEx::PrintfInternal(
-	__in PCHAR	pFuncName,
-	__in LPSTR	Fmt,
+	__in LPTSTR			lpMod,
+	__in PRINTF_LEVEL	PrintfLevel,
+	__in LPSTR			pFuncName,
+	__in LPSTR			Fmt,
 	...
 	)
 {
-	CHAR chInfo[MAX_PATH] = {0};
+	time_t	rawTime				= 0;
+	tm		timeInfo			= {0};
+	CHAR	chFmtInfo[MAX_PATH]	= {0};
 
 	va_list Args;
 
 
-	setlocale(LC_ALL, "");
+	__try
+	{
+		va_start(Args, Fmt);
 
-	va_start(Args, Fmt);
+		setlocale(LC_ALL, "");
 
-	StringCchVPrintfA(chInfo, _countof(chInfo), Fmt, Args);
+		// 时间
+		time(&rawTime);
+		localtime_s(&timeInfo, &rawTime);
 
-	printf("[%hs] %hs \n", pFuncName, chInfo);
+		StringCchVPrintfA(chFmtInfo, _countof(chFmtInfo), Fmt, Args);
 
-	va_end(Args);
+		// 时间 线程 模块 函数 信息
+		printf("[%04d/%02d/%02d][%02d:%02d:%02d][%05d][%lS][%hs] %hs \n", 
+			timeInfo.tm_year + 1900,
+			timeInfo.tm_mon + 1,
+			timeInfo.tm_mday,
+			timeInfo.tm_hour,
+			timeInfo.tm_min,
+			timeInfo.tm_sec,
+			GetCurrentThreadId(),
+			lpMod ? lpMod : _T("未指定"),
+			pFuncName,
+			chFmtInfo
+			);
+	}
+	__finally
+	{
+		va_end(Args);
+	}
 }
--- PrintfEx/PrintfEx.h
+++ PrintfEx/PrintfEx.h
@@ -1,10 +1,21 @@
 #pragma once
 
 #include <Windows.h>
+#include <tchar.h>
 #include <strsafe.h>
 #include <locale.h>
+#include <time.h>
 
 #define MOD_PRINTF_EX _T("PrintfEx")
+
+typedef enum _PRINTF_LEVEL
+{
+	PRINTF_LEVEL_INFORMATION,
+	PRINTF_LEVEL_WARNING,
+	PRINTF_LEVEL_ERROR
+} PRINTF_LEVEL, *PPRINTF_LEVEL, *LPPRINTF_LEVEL;
+
+#define printfEx(lpMod, PrintfLevel, FMT, ...) CPrintfEx::PrintfInternal(lpMod, PrintfLevel, __FUNCTION__, FMT, __VA_ARGS__)
 
 class CPrintfEx
 {
@@ -12,10 +23,10 @@ public:
 	static
 		VOID
 		PrintfInternal(
-		__in PCHAR	pFuncName,
-		__in LPSTR	Fmt,
+		__in LPTSTR			lpMod,
+		__in PRINTF_LEVEL	PrintfLevel,
+		__in LPSTR			pFuncName,
+		__in LPSTR			Fmt,
 		...
 		);
 };
-
-#define printfEx(FMT, ...) CPrintfEx::PrintfInternal(__FUNCTION__, FMT, __VA_ARGS__)
