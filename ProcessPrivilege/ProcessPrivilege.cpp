da5c82d2c6e236aec5414f2d1016a8ea0f64da85
yuexiang <yuexiang@cherilead.com>
2015-08-21 17:50:27 +0800

    进程权限

---
+++ ProcessPrivilege/ProcessPrivilege.cpp
@@ -0,0 +1,63 @@
+#include "ProcessPrivilege.h"
+
+BOOL
+	CProcessPrivilege::Adjust(
+	__in BOOL	bCurrentProcess,
+	__in ULONG	ulPid,
+	__in LPTSTR lpName
+	)
+{
+	BOOL				bRet			= FALSE;
+
+	HANDLE				hProcess		= NULL;
+	HANDLE				hToken			= NULL;
+	TOKEN_PRIVILEGES	TokenPrivileges = {0};
+
+
+	__try
+	{
+		if (!bCurrentProcess && !ulPid)
+		{
+			printfEx(MOD_PROCESS_PRIVILEGE, PRINTF_LEVEL_ERROR, "input arguments error");
+			__leave;
+		}
+
+		if (bCurrentProcess)
+			hProcess = GetCurrentProcess();
+		else
+			;
+
+		if (!OpenProcessToken(hProcess, TOKEN_ADJUST_PRIVILEGES, &hToken))
+		{
+			printfEx(MOD_PROCESS_PRIVILEGE, PRINTF_LEVEL_ERROR, "OpenProcessToken failed. (%d)", GetLastError());
+			__leave;
+		}
+
+		if (!LookupPrivilegeValue(NULL, lpName, &TokenPrivileges.Privileges[0].Luid))
+		{
+			printfEx(MOD_PROCESS_PRIVILEGE, PRINTF_LEVEL_ERROR, "LookupPrivilegeValue failed. (%d)", GetLastError());
+			__leave;
+		}
+
+		TokenPrivileges.PrivilegeCount = 1;
+		TokenPrivileges.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+		if (!AdjustTokenPrivileges(hToken, FALSE, &TokenPrivileges, sizeof(TokenPrivileges), NULL, NULL))
+		{
+			printfEx(MOD_PROCESS_PRIVILEGE, PRINTF_LEVEL_ERROR, "AdjustTokenPrivileges failed. (%d)", GetLastError());
+			__leave;
+		}
+
+		bRet = TRUE;
+	}
+	__finally
+	{
+		if (hToken)
+		{
+			CloseHandle(hToken);
+			hToken = NULL;
+		}
+	}
+
+	return bRet;
+}
