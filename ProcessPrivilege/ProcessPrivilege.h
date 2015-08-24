3ee9cbf78b2fbd34c773157af093ecf617509dcd
fengkuangfj <fly464447966@126.com>
2015-08-24 21:56:47 +0800

    添加ProcessPrivilege

---
+++ ProcessPrivilege/ProcessPrivilege.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <Windows.h>
+
+#include "..\\PrintfEx\\PrintfEx.h"
+
+#define MOD_PROCESS_PRIVILEGE _T("进程权限")
+
+class CProcessPrivilege
+{
+public:
+	static
+		BOOL
+		Adjust(
+		__in BOOL	bCurrentProcess,
+		__in ULONG	ulPid,
+		__in LPTSTR lpName
+		);
+};
