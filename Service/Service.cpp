634e1881e5ab7d56b658e90fc0e1c5aca15bc642
yuexiang <yuexiang@cherilead.com>
2015-09-10 17:18:02 +0800

    允许服务接收SERVICE_ACCEPT_POWEREVENT等事件

--- Service/Service.cpp
+++ Service/Service.cpp
@@ -852,7 +852,18 @@ VOID
 		if (SERVICE_START_PENDING == dwCurrentState)
 			ms_SvcStatus.dwControlsAccepted = 0;
 		else
-			ms_SvcStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+			ms_SvcStatus.dwControlsAccepted =
+			SERVICE_ACCEPT_STOP                  |  // 0x00000001
+			SERVICE_ACCEPT_PAUSE_CONTINUE        |  // 0x00000002
+			SERVICE_ACCEPT_SHUTDOWN              |  // 0x00000004
+			SERVICE_ACCEPT_PARAMCHANGE           |  // 0x00000008
+			SERVICE_ACCEPT_NETBINDCHANGE         |  // 0x00000010
+			SERVICE_ACCEPT_HARDWAREPROFILECHANGE |  // 0x00000020
+			SERVICE_ACCEPT_POWEREVENT            |  // 0x00000040
+			SERVICE_ACCEPT_SESSIONCHANGE         |  // 0x00000080
+			SERVICE_ACCEPT_PRESHUTDOWN           |  // 0x00000100
+			SERVICE_ACCEPT_TIMECHANGE            |  // 0x00000200
+			SERVICE_ACCEPT_TRIGGEREVENT;            // 0x00000400
 
 		if ((SERVICE_RUNNING == dwCurrentState) || (SERVICE_STOPPED == dwCurrentState))
 			ms_SvcStatus.dwCheckPoint = 0;
