743282dda7570c41370813334d851e361dd1bdfe
fengkuangfj <fly464447966@126.com>
2017-01-04 18:49:37 +0800

    更新bat

--- 清理当前VS项目目录下的非必要文件.bat
+++ 清理当前VS项目目录下的非必要文件.bat
@@ -4,48 +4,6 @@ if not ""=="%1" (
 cd /d "%1"
 )
 
-del /f /s /q /a *.aps 2>nul
-del /f /s /q /a *.backup 2>nul
-del /f /s /q /a *.cache 2>nul
-del /f /s /q /a *.db 2>nul
-del /f /s /q /a *.db-journal 2>nul
-del /f /s /q /a *.dep 2>nul
-del /f /s /q /a *.err 2>nul
-del /f /s /q /a *.error 2>nul
-rem del /f /s /q /a *.exp 2>nul
-del /f /s /q /a *.htm 2>nul
-del /f /s /q /a *.idb 2>nul
-del /f /s /q /a *.ilk 2>nul
-del /f /s /q /a *.iobj 2>nul
-del /f /s /q /a *.ipch 2>nul
-del /f /s /q /a *.ipdb 2>nul
-del /f /s /q /a *.lastbuildstatus 2>nul
-del /f /s /q /a *.lnk 2>nul
-del /f /s /q /a *.log 2>nul
-del /f /s /q /a *.mac 2>nul
-rem del /f /s /q /a *.manifest 2>nul
-del /f /s /q /a *.ncb 2>nul
-del /f /s /q /a *.obj 2>nul
-del /f /s /q /a *.opensdf 2>nul
-del /f /s /q /a *.pch 2>nul
-del /f /s /q /a *.pdb 2>nul
-del /f /s /q /a *.pml 2>nul
-del /f /s /q /a *.res 2>nul
-rem del /f /s /q /a *.scc 2>nul
-del /f /s /q /a *.sdf 2>nul
-del /f /s /q /a *.suo 2>nul
-del /f /s /q /a *.svn-bast 2>nul
-del /f /s /q /a *.tlb 2>nul
-del /f /s /q /a *.tlog 2>nul
-del /f /s /q /a *.unsuccessfulbuild 2>nul
-del /f /s /q /a *.user 2>nul
-del /f /s /q /a *.vddklaunch 2>nul
-rem del /f /s /q /a *.vspscc 2>nul
-rem del /f /s /q /a *.vsscc 2>nul
-rem del /f /s /q /a *.vssscc 2>nul
-del /f /s /q /a *.wrn 2>nul
-rem del /f /s /q /a *.xml 2>nul
-
 for /r . %%d in (.) do rd /s /q "%%d\objchk_win7_amd64" 2>nul
 for /r . %%d in (.) do rd /s /q "%%d\objchk_win7_ia64" 2>nul
 for /r . %%d in (.) do rd /s /q "%%d\objchk_win7_x86" 2>nul
@@ -88,4 +46,46 @@ for /r . %%d in (.) do rd /s /q "%%d\obj" 2>nul
 rem for /r . %%d in (.) do rd /s /q "%%d\Package" 2>nul
 for /r . %%d in (.) do rd /s /q "%%d\Unicode Debug" 2>nul
 for /r . %%d in (.) do rd /s /q "%%d\Unicode Release" 2>nul
-for /r . %%d in (.) do rd /s /q "%%d\x64" 2>nul
+rem for /r . %%d in (.) do rd /s /q "%%d\x64" 2>nul
+
+del /f /s /q /a *.aps 2>nul
+del /f /s /q /a *.backup 2>nul
+del /f /s /q /a *.cache 2>nul
+del /f /s /q /a *.db 2>nul
+del /f /s /q /a *.db-journal 2>nul
+del /f /s /q /a *.dep 2>nul
+del /f /s /q /a *.err 2>nul
+del /f /s /q /a *.error 2>nul
+rem del /f /s /q /a *.exp 2>nul
+del /f /s /q /a *.htm 2>nul
+del /f /s /q /a *.idb 2>nul
+del /f /s /q /a *.ilk 2>nul
+del /f /s /q /a *.iobj 2>nul
+del /f /s /q /a *.ipch 2>nul
+del /f /s /q /a *.ipdb 2>nul
+del /f /s /q /a *.lastbuildstatus 2>nul
+del /f /s /q /a *.lnk 2>nul
+del /f /s /q /a *.log 2>nul
+del /f /s /q /a *.mac 2>nul
+rem del /f /s /q /a *.manifest 2>nul
+del /f /s /q /a *.ncb 2>nul
+del /f /s /q /a *.obj 2>nul
+del /f /s /q /a *.opensdf 2>nul
+del /f /s /q /a *.pch 2>nul
+rem del /f /s /q /a *.pdb 2>nul
+del /f /s /q /a *.pml 2>nul
+del /f /s /q /a *.res 2>nul
+rem del /f /s /q /a *.scc 2>nul
+del /f /s /q /a *.sdf 2>nul
+del /f /s /q /a *.suo 2>nul
+del /f /s /q /a *.svn-bast 2>nul
+del /f /s /q /a *.tlb 2>nul
+del /f /s /q /a *.tlog 2>nul
+del /f /s /q /a *.unsuccessfulbuild 2>nul
+del /f /s /q /a *.user 2>nul
+del /f /s /q /a *.vddklaunch 2>nul
+rem del /f /s /q /a *.vspscc 2>nul
+rem del /f /s /q /a *.vsscc 2>nul
+rem del /f /s /q /a *.vssscc 2>nul
+del /f /s /q /a *.wrn 2>nul
+rem del /f /s /q /a *.xml 2>nul
