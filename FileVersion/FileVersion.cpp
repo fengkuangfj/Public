6258d62250945f1a87469002b1d09533ab6aaefc
fengkuangfj <fly464447966@126.com>
2017-05-22 19:07:30 +0800

    更新

--- FileVersion/FileVersion.cpp
+++ FileVersion/FileVersion.cpp
@@ -1,2 +1,101 @@
 
-#
+#include "FileVersion.h"
+
+CFileVersion::CFileVersion()
+{
+	;
+}
+
+CFileVersion::~CFileVersion()
+{
+	;
+}
+
+BOOL
+CFileVersion::Get(
+				  __in		LPCTSTR	lpPath,
+				  __inout	LPTSTR	lpVersion,
+				  __in		ULONG	ulBufSizeCh
+				  )
+{
+	BOOL				bRet = FALSE;
+
+	FILE_VERSION_INFO	FileVersionInfo = {0};
+
+
+	__try
+	{
+		if (!lpPath || !lpVersion || !ulBufSizeCh)
+			__leave;
+
+		if (!Get(lpPath, &FileVersionInfo))
+			__leave;
+
+		StringCchPrintf(lpVersion, ulBufSizeCh, _T("%d.%d.%d.%d"),
+			FileVersionInfo.ulMajorVersion,
+			FileVersionInfo.ulMinorVersion,
+			FileVersionInfo.ulPatchVersion,
+			FileVersionInfo.ulBuildVersion
+			);
+
+		bRet = TRUE;
+	}
+	__finally
+	{
+		;
+	}
+
+	return bRet;
+}
+
+BOOL
+CFileVersion::Get(
+				  __in		LPCTSTR				lpPath,
+				  __inout	LPFILE_VERSION_INFO lpFileVersionInfo
+				  )
+{
+	BOOL				bRet = FALSE;
+
+	DWORD				dwResult = 0;
+	LPVOID				lpData = NULL;
+	VS_FIXEDFILEINFO *	pVsFixedFileInfo = NULL;
+	UINT				nLen = 0;
+
+
+	__try
+	{
+		if (!lpPath || !lpFileVersionInfo)
+			__leave;
+
+		dwResult = GetFileVersionInfoSize(lpPath, NULL);  
+		if (!dwResult)
+			__leave;
+
+		lpData = calloc(1, dwResult);
+		if (!lpData)
+			__leave;
+
+		if (!GetFileVersionInfo(lpPath, 0, dwResult, lpData))
+			__leave;
+
+		if (!VerQueryValue(lpData, _T("\\"), (LPVOID *)&pVsFixedFileInfo, &nLen))
+			__leave;
+
+		lpFileVersionInfo->ulMajorVersion = HIWORD(pVsFixedFileInfo->dwFileVersionMS);
+		lpFileVersionInfo->ulMinorVersion = LOWORD(pVsFixedFileInfo->dwFileVersionMS);
+		lpFileVersionInfo->ulPatchVersion = HIWORD(pVsFixedFileInfo->dwFileVersionLS);
+		lpFileVersionInfo->ulBuildVersion = LOWORD(pVsFixedFileInfo->dwFileVersionLS);
+
+		bRet = TRUE;
+	}
+	__finally
+	{
+		if (lpData)
+		{
+			free(lpData);
+			lpData = NULL;
+		}
+	}
+
+	return bRet;
+}
--- FileVersion/FileVersion.h
+++ FileVersion/FileVersion.h
@@ -1,21 +1,37 @@
 
 #pragma once
 
+#include <Windows.h>
+#include <tchar.h>
+#include <strsafe.h>
+
+#pragma comment(lib, "Version.lib")
+
 typedef struct _FILE_VERSION_INFO
 {
-	ULONG ul;
-	ULONG ul
-		ULONG ul;
-	ULONG ul;
-
+	ULONG ulMajorVersion;
+	ULONG ulMinorVersion;
+	ULONG ulPatchVersion;
+	ULONG ulBuildVersion;
 } FILE_VERSION_INFO, *PFILE_VERSION_INFO, *LPFILE_VERSION_INFO;
 
 class CFileVersion
 {
 public:
+	static
+		BOOL
+		Get(
+		__in	LPCTSTR	lpPath,
+		__inout LPTSTR	lpVersion,
+		__in	ULONG	ulBufSizeCh
+		);
 
-
-
+	static
+		BOOL
+		Get(
+		__in	LPCTSTR				lpPath,
+		__inout LPFILE_VERSION_INFO lpFileVersionInfo
+		);
 
 private:
 	CFileVersion();
